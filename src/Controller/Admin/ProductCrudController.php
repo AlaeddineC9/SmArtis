6a7a1d0acaa48a0fb3e1b8984953c29bd5c26a63
Alaeddine c9 <alaeddinechraiti@gmail.com>
2024-12-08 21:17:43 +0100

    Refactor CSS styles and add image width to navbar and footer
    - In styles.css, added width: 100% to the .navbar class and .footer class to ensure they span the entire width of the page.
    - Updated the image source in index.html.twig for both the product and category templates to use the asset() function and include the 'uploads/products/' prefix.
    - Adjusted the margin-left style for the category and souscategory names in index.html.twig to align them properly.
    - Updated the text color in the footer to be dark instead of light.

--- src/Controller/Admin/ProductCrudController.php
+++ src/Controller/Admin/ProductCrudController.php
@@ -3,8 +3,6 @@
 namespace App\Controller\Admin;
 
 use App\Entity\Product;
-
-
 use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
 use EasyCorp\Bundle\EasyAdminBundle\Field\SlugField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
@@ -15,7 +13,6 @@ use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 use EasyCorp\Bundle\EasyAdminBundle\Field\BooleanField;
-
 
 class ProductCrudController extends AbstractCrudController
 {
@@ -28,36 +25,52 @@ class ProductCrudController extends AbstractCrudController
     {
         return $crud
             ->setEntityLabelInSingular('Produit')
-            ->setEntityLabelInPlural('Produits');
+            ->setEntityLabelInPlural('Produits')
+            ->setPageTitle(Crud::PAGE_INDEX, 'Gestion des Produits')
+            ->setPageTitle(Crud::PAGE_NEW, 'Créer un Produit')
+            ->setPageTitle(Crud::PAGE_EDIT, 'Modifier un Produit')
+            ->setPageTitle(Crud::PAGE_DETAIL, 'Détail du Produit');
     }
-
 
     public function configureFields(string $pageName): iterable
     {
         $required = true;
 
-        if ($pageName == 'edit') {
+        if ($pageName === Crud::PAGE_EDIT) {
             $required = false;
-        };
+        }
+
         return [
-            TextField::new('name')->setLabel('Nom')->setHelp('Nom de votre produit'),
+            TextField::new('name')
+                ->setLabel('Nom')
+                ->setHelp('Nom de votre produit'),
+
             BooleanField::new('isHomepage')
-    ->setLabel('Produit à la une ?')
-    ->setHelp('Mettre en avant ce produit sur la page d\'accueil')
-    ->renderAsSwitch(true),
-     // Utilisez renderAsSwitch si vous voulez un interrupteur, sinon laissez par défaut
-            SlugField::new('slug')->setTargetFieldName('name')->setLabel('URL')->setHelp('URL de votre produit'),
-            TextEditorField::new('description')->setLabel('Description')->setHelp('Description de votre produit'),
+                ->setLabel('Produit à la une ?')
+                ->setHelp('Mettre en avant ce produit sur la page d\'accueil')
+                ->renderAsSwitch(),
+
+            SlugField::new('slug')
+                ->setTargetFieldName('name')
+                ->setLabel('URL')
+                ->setHelp('URL de votre produit'),
+
+            TextEditorField::new('description')
+                ->setLabel('Description')
+                ->setHelp('Description de votre produit'),
+
             ImageField::new('illustration')
                 ->setLabel('Image')
                 ->setHelp('Image de votre produit en 600x600px')
-                ->setUploadedFileNamePattern('[year]-[month]-[day]-[contenthash].[extention]')
-                ->setBasePath('/uploads')
-                ->setUploadDir('/public/uploads')
+                ->setUploadedFileNamePattern('[year]-[month]-[day]-[contenthash].[extension]')
+                ->setBasePath('/uploads/products')
+                ->setUploadDir('public/uploads/products')
                 ->setRequired($required),
+
             NumberField::new('price')
                 ->setLabel('Prix H.T')
                 ->setHelp('Prix H.T de votre produit sans le sigle €'),
+
             ChoiceField::new('tva')
                 ->setLabel('Taux de TVA')
                 ->setChoices([
@@ -67,8 +80,11 @@ class ProductCrudController extends AbstractCrudController
                     '20%' => '20',
                 ]),
 
-            AssociationField::new('category', 'Catégorie associée'),
-            AssociationField::new('sousCategory', 'Sous-catégorie associée'),
+            AssociationField::new('category', 'Catégorie associée')
+                ->autocomplete(),
+
+            AssociationField::new('sousCategory', 'Sous-catégorie associée')
+                ->autocomplete(),
         ];
     }
 }
